16a4e1a4fe9e0dd181a1c8e2535420fdf7a022b4
Madhushan Madhushan <ramishka1998@gmail.com>
2026-03-12 10:11:50 +0530

    style: apply monochrome slate style to partner logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -536,31 +536,31 @@ export default function HomePage() {
             <img 
               src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" 
               alt="Amazon Web Services" 
-              className="h-10 sm:h-12 md:h-16 w-auto object-contain hover:scale-105 transition-transform" 
+              className="h-10 sm:h-12 md:h-16 w-auto object-contain brightness-0 opacity-60 hover:opacity-100 transition-all duration-300" 
             />
             {/* Google Cloud */}
             <img 
               src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecloud/googlecloud-original-wordmark.svg" 
               alt="Google Cloud" 
-              className="h-10 sm:h-12 md:h-16 w-auto object-contain hover:scale-105 transition-transform" 
+              className="h-10 sm:h-12 md:h-16 w-auto object-contain brightness-0 opacity-60 hover:opacity-100 transition-all duration-300" 
             />
             {/* Azure */}
             <img 
               src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azure/azure-original-wordmark.svg" 
               alt="Microsoft Azure" 
-              className="h-10 sm:h-12 md:h-16 w-auto object-contain hover:scale-105 transition-transform" 
+              className="h-10 sm:h-12 md:h-16 w-auto object-contain brightness-0 opacity-60 hover:opacity-100 transition-all duration-300" 
             />
             {/* Salesforce */}
             <img 
               src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Salesforce.com_logo.svg" 
               alt="Salesforce" 
-              className="h-12 sm:h-16 md:h-20 w-auto object-contain hover:scale-105 transition-transform" 
+              className="h-12 sm:h-16 md:h-20 w-auto object-contain brightness-0 opacity-60 hover:opacity-100 transition-all duration-300" 
             />
             {/* Kubernetes */}
             <img 
               src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/kubernetes/kubernetes-plain-wordmark.svg" 
               alt="Kubernetes" 
-              className="h-10 sm:h-12 md:h-16 w-auto object-contain hover:scale-105 transition-transform" 
+              className="h-10 sm:h-12 md:h-16 w-auto object-contain brightness-0 opacity-60 hover:opacity-100 transition-all duration-300" 
             />
           </div>
         </div>
